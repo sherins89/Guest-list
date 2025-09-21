ec20ba5fecbcbcfc68fbb3c9b63ee394fc1b4f6f
Sherin <sherin.daniel89@yahoo.com>
2025-09-21 01:02:53 -0400

    components added

--- src/api/guests.js
+++ src/api/guests.js
@@ -2,3 +2,16 @@
 const BASE_URL = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/";
 const COHORT = "2506-ftb-ct-web-pt";
 const API = BASE_URL + COHORT;
+
+// Get all Guest //
+
+export async function getGuests() {
+  try {
+    const response = await fetch(API + "/guests");
+    const result = await response.json();
+    return result.data;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
+}
